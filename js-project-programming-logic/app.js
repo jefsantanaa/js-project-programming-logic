d5b63db6e6cfcf68fe49fd3363e8780960912bcd
Jefferson Santana <jeffersonsantana1914@outlook.com>
2024-07-03 20:04:01 -0300

    Learning about ternary

--- js-project-programming-logic/app.js
+++ js-project-programming-logic/app.js
@@ -1,21 +1,26 @@
-alert('Hi there! you are on the Secret Number Game');
-let secretNumber = 29;
-let tries = 1;
+alert('Hi there! You are on the Secret Number Game');
+let maximumNumber = 10;
+let secretNumber = parseInt(Math.random() * maximumNumber + 1);
+let attempt = 1;
 let guess;
 
-console.log('Secret number is: ' + secretNumber);
+console.log('Secret Number is: ' + secretNumber);
 
 while (guess != secretNumber) {
-  guess = prompt('Choose a number from 1 to 30:');
-
+  guess = prompt(`Choose a number from 1 to ${maximumNumber}:`);
   if (guess == secretNumber) {
-    alert(`Congratulations, you got the number right, it\'s ${secretNumber}! Times tried: ${tries}`);
+    break;
   } else {
     if (guess > secretNumber) {
-      alert('The secret number is smaller than the guessed number '+ guess);
+      alert('The Secret Number is smaller than the Guessed Number '+ guess);
     } else {
-      alert('The secret number is greater than the guessed number ' + guess);
+      alert('The Secret Number is greater than the Guessed Number ' + guess);
     }
-    tries++;
+    attempt++;
   }
 }
+
+let wordAttemptInPlural = attempt > 1 ? 'attempts' : 'attempt'; /* Is attempts greater than 1? If attempt is greater than 1 write "attempts", otherwise "attempt" */
+alert(`Congratulations! You Guessed the Secret Number is ${secretNumber} with ${attempt} ${wordInPlural}.`);
+
+
---
+++ js-project-programming-logic/challenges4.js
@@ -0,0 +1,61 @@
+console.log('Welcome!'); // Exercise 1
+
+let name = 'Jefferson Santana'; // Exercise 2
+console.log(`Hello! ${name}.`);
+
+let name = 'Jefferson Santana'; // Exercise 3
+alert(`Hello! ${name}`);
+
+let answer = prompt('What\'s your favorite programming language?'); // Exercise 4
+console.log(answer);
+
+let value1 = 2; // Exercise 5
+let value2 = 3;
+let result = value1 + value2;
+console.log(`The sum of ${value1} and ${value2} is ${result}!`);
+
+let value1 = 5; // Exercise 6
+let value2 = 2;
+let result = value1 - value2;
+console.log(`The subtraction of ${value1} and ${value2} is ${result}!`);
+
+let age = prompt('How old are you?'); // Exercise 7
+if (age > 17) {
+  console.log('You are of legal age!');
+} else {
+  console.log('You are not of legal age!');
+}
+
+let num = prompt('Give me a number:'); // Exercise 8
+if (num > 0) {
+  console.log('This number is posite!');
+} else if (num < 0) {
+  console.log('This number is negative!');
+} else {
+  console.log('This number is zero!');
+}
+
+let num = 1; // Exercise 9
+while (num <= 10) {
+  console.log(num);
+  num++;
+}
+
+let grade = 7; // Exercise 10
+if (grade >= 7) {
+  console.log('Approved!');
+} else {
+  console.log('Reapproved');
+}
+
+let randomNum = Match.random(); // Exercise 11
+console.log(randomNum);
+
+let randomNum = parseInt(Match.random() * 10) + 1; // Exercise 12
+console.log(randomNum);
+
+let randomNum = parseInt(Match.random() * 1000) + 1; // Exercise 13
+console.log(randomNum);
+
+
+
