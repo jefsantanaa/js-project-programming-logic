9267dd2344c0c7dd41cf70fe1d1460b12df72f86
Jefferson Santana <jeffersonsantana1914@outlook.com>
2024-07-02 22:53:24 -0300

    Learning about flow control

--- js-project-programming-logic/app.js
+++ js-project-programming-logic/app.js
@@ -1,14 +1,21 @@
 alert('Hi there! you are on the Secret Number Game');
 let secretNumber = 29;
-console.log('Secret number is: ' + secretNumber)
-let guess = prompt('Choose a number from 1 to 30:');
-console.log('Guess number: ' + guess);
+let tries = 1;
+let guess;
 
-console.log('Comparison between guess and secrect number: ' + guess == secretNumber);
-if (guess == secretNumber) {
-  console.log(`Congratulations, you got the number right, it\'s ${secretNumber}!`);
-  alert(`Congratulations, you got the number right, it\'s ${secretNumber}!`);
-} else {
-  console.log('Secreat number is ' + secretNumber);
-  alert('Try again! You missed.');
+console.log('Secret number is: ' + secretNumber);
+
+while (guess != secretNumber) {
+  guess = prompt('Choose a number from 1 to 30:');
+
+  if (guess == secretNumber) {
+    alert(`Congratulations, you got the number right, it\'s ${secretNumber}! Times tried: ${tries}`);
+  } else {
+    if (guess > secretNumber) {
+      alert('The secret number is smaller than the guessed number '+ guess);
+    } else {
+      alert('The secret number is greater than the guessed number ' + guess);
+    }
+    tries++;
+  }
 }
---
+++ js-project-programming-logic/challenges3.js
@@ -0,0 +1,24 @@
+let count = 1; // Exercise 1
+while (count <= 10) { 
+  console.log(count);
+  count++;
+}
+
+let count2 = 10; // Exercise 2
+while (count2 >= 0) { 
+  console.log(count2);
+  count2--;
+}
+
+let num = prompt('Give me a number:'); // Exercise 3
+while (num >= 0) {
+  console.log(num);
+  num--;
+}
+
+let num2 = prompt('Give me a number:'); // Exercise 4
+let count3 = 0;
+while (count3 <= num2) {
+  console.log(count3);
+  count3++;
+}
